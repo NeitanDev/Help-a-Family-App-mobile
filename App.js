4e5edf1d80768589425f9e383e70cf3ebfc46fea
NeitanDev <natan.miranda02@gmail.com>
2020-04-08 02:04:43 -0300

    Teste de mapa com marker

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  StatusBar
-} from 'react-native';
 import * as Font from 'expo-font';
-//import Index from './src/index';
+import Index from './src/index';
 
 export default class App extends React.Component {
   state = {
@@ -25,9 +20,7 @@ export default class App extends React.Component {
         }
         {
           this.state.fontLoaded ? (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: '#2bbf54' }}>
-              <Text style={{ fontSize: 56, fontFamily: 'Amaranth-Regular', color: '#FFF' }}>Hello Word</Text>
-            </View>
+            <Index />
           ) : null
         }
       </>
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default function Index() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+    const [latitude, setLatitude] = useState(-23.2730918);
+    const [longitude, setLongitude] = useState(-46.5902931);
+
+    useEffect(() => {
+        async function loadPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                })
+            }
+        }
+        loadPosition();
+    }, []);
+
+
+    return (
+        <View style={styles.container}>
+            <MapView initialRegion={currentRegion} style={styles.mapStyle}>
+                <Marker coordinate={{ latitude, longitude }}>
+
+                </Marker>
+            </MapView>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight,
+    },
+    mapStyle: {
+        flex: 1,
+    },
+});
