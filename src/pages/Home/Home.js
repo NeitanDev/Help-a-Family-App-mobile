415244123f904694f3fc9c0746478b50d5e9de22
NeitanDev <natan.miranda02@gmail.com>
2020-04-08 20:41:04 -0300

    Em desenvolvimento

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View, Text,Image } from 'react-native';
-import Logo from '../../../assets/sounou.png';
+import { View, Text, Image } from 'react-native';
+import Logosounou from '../../assets/LogoVerde.png';
+
+import { Container, Header, Logo } from './styles/style';
 
 
 export default function Home() {
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: 'center', backgroundColor: '#2bbf54' }}>
-            <Image source={Logo} />
+        <Container>
+            <Header>
+                <View style={{backgroundColor: '#333',height:'100%',width:'50%', alignItems:'center'}}>
+                    <Logo source={Logosounou} />
+                </View>
+            </Header>
             <Text style={{ fontFamily: 'Amaranth-Regular', fontSize: 56, color: '#FFF' }}>Home</Text>
-        </View>
+        </Container>
+
     );
 }
--- src/pages/Home/styles/style.js
+++ src/pages/Home/styles/style.js
@@ -1,1 +1,19 @@
 import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex:1;
+    align-items: center;
+    background-color: #2bbf54;
+`;
+
+export const Header = styled.View`
+    width:100%;
+    align-items: center;
+    height: 20%;
+    background-color: #2bbf54;
+`;
+
+export const Logo = styled.Image`
+    height: 100%;
+    width:100%;
+`;
