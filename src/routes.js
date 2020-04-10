7a6404e8610b20407966c22a6ac912cde40a31bb
NeitanDev <natan.miranda02@gmail.com>
2020-04-09 21:42:33 -0300

    Rotas de Login e cadastro feito

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './pages/Home/Home';
 import LoginFamilia from './pages/Familia/Login/index';
 import LoginOrg from './pages/Organizacao/Login/index';
+import CadastroFamilia from './pages/Familia/Cadastro/index';
+import CadastroOrg from './pages/Organizacao/Cadastro/index';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ function Routes() {
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="LoginFamilia" component={LoginFamilia} />
                 <Stack.Screen name="LoginOrg" component={LoginOrg} />
+                <Stack.Screen name="CadastroFamilia" component={CadastroFamilia} />
+                <Stack.Screen name="CadastroOrg" component={CadastroOrg} />
             </Stack.Navigator>
         </NavigationContainer>
     );
