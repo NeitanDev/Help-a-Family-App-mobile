b7c61be73eee54de61b7d9f2fb6d7c5d295973aa
NeitanDev <natan.miranda02@gmail.com>
2020-04-10 22:29:10 -0300

    icons na tabar feitos

--- src/pages/Organizacao/routes.js
+++ src/pages/Organizacao/routes.js
@@ -4,28 +4,50 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Profile from './Profile/index';
 import Historico from './Historico/index';
 import Maps from './Map/index';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesing from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 
 export default function Org() {
     return (
         <Tab.Navigator
-        tabBarOptions={{
-            activeTintColor: '#FFF',
-            inactiveTintColor: 'gray',
-            activeBackgroundColor: '#2bbf54',
-            inactiveBackgroundColor: '#2bbf54',
-            style: {
-                borderTopWidth:0,
-                shadowColor: '#2bbf54',
-                shadowOpacity: 0,
-                shadowOffset: {
-                    width: 0,
-                    height: 0,
+            tabBarOptions={{
+                activeTintColor: '#c1f1ce',
+                inactiveTintColor: '#1c7d38',
+                activeBackgroundColor: '#2bbf54',
+                inactiveBackgroundColor: '#2bbf54',
+                style: {
+                    borderTopWidth: 0,
+                    shadowColor: '#2bbf54',
+                    shadowOpacity: 0,
+                    shadowOffset: {
+                        width: 0,
+                        height: 0,
+                    },
+                    elevation: 0,
                 },
-                elevation: 0,
-            },
-          }}
+            }}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Profile') {
+                        iconName = 'profile';
+                        return <AntDesing name={iconName} size={size} color={color} />;
+                    } else if (route.name === 'Historico') {
+                        iconName = 'history';
+                        return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+                    } else if (route.name === 'Map') {
+                        iconName = 'google-maps';
+                        return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+                    }
+
+                    // You can return any component that you like here!
+                    
+                },
+            })}
+
         >
             <Tab.Screen name="Profile" component={Profile} />
             <Tab.Screen name="Historico" component={Historico} />
